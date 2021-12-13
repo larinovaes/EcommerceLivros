0619564b71fec05e8812d81e5edc2a6829dc1eb6
Larissa Novais <larinovais54@gmail.com>
2021-12-13 14:14:00 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/zup/ecommerceLivros/EcommerceLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ecommerceLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/ecommerceLivros/EcommerceLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ecommerceLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
